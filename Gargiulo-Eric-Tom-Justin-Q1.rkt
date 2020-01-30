8274c736042da4f18b404010ecea685348a28def
gargiuer <54644109+gargiuer@users.noreply.github.com>
2020-01-29 19:50:06 -0500

    Update Gargiulo-Eric-Tom-Justin-Q1.rkt

--- Gargiulo-Eric-Tom-Justin-Q1.rkt
+++ Gargiulo-Eric-Tom-Justin-Q1.rkt
@@ -1,5 +1,6 @@
-#lang eopl
+#lang racket
 (require fsm)
+(require test-engine/racket-tests)
 
 (define QUIZ
   (make-dfa '(Q0 Q1 Q2)
@@ -12,18 +13,28 @@
               (Q1 a Q1)
               (Q1 b Q0))))
 
-(sm-getrules QUIZ)
+(define member?
+  (lambda (x lst)
+    (cond
+      [(empty? lst) #f]
+      [(eqv? x (car lst)) #t]
+      [else (member? x (cdr lst))])))
 
+(check-expect (member? 3 '(1 2 3)) #t)
+(check-expect (member? 4 '(1 2 3)) #f)
 
 ; x is a list of rules
 ; res is a list of start and final states
-(define unreachable
+(define reachable
   (lambda (x res)
     (cond
-      [(empty? x) empty]
-      [(and (member? (car x) res) (not (member? (caddr x) res))) (unreachable (cdr x) (cons (caddr x) res))])
+      [(null? x) '()]
+      [(and (member? (caar x) res) (not (member? (caddar x) res))) (reachable (cdr x) (cons (caddar x) res))]
+      [else (reachable (cdr x) res)])
     res))
 
 
 
-(unreachable (sm-getrules QUIZ) (sm-getstartstate QUIZ))
+(check-expect (reachable (sm-getrules QUIZ) (list (sm-getstart QUIZ))) '(Q0 Q1))
+
+(test)
