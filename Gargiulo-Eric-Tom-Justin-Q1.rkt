b01585e80d9a7fecaedf616e7106348555b5f3fb
gargiuer <54644109+gargiuer@users.noreply.github.com>
2020-01-29 19:16:08 -0500

    Update Gargiulo-Eric-Tom-Justin-Q1.rkt

--- Gargiulo-Eric-Tom-Justin-Q1.rkt
+++ Gargiulo-Eric-Tom-Justin-Q1.rkt
@@ -1,12 +1,29 @@
-#lang racket
+#lang eopl
 (require fsm)
 
 (define QUIZ
-  (make-dfa '(q0 q1)
+  (make-dfa '(Q0 Q1 Q2)
             '(a b)
-            'q0
-            '(q0)
-            '((q0 a q0)
-              (q0 b q1)
-              (q1 a q1)
-              (q1 b q0))))
+            'Q0
+            '(Q0)
+            '((Q2 a Q2)
+              (Q0 a Q0)
+              (Q0 b Q1)
+              (Q1 a Q1)
+              (Q1 b Q0))))
+
+(sm-getrules QUIZ)
+
+
+; x is a list of rules
+; res is a list of start and final states
+(define unreachable
+  (lambda (x res)
+    (cond
+      [(empty? x) empty]
+      [(and (member? (car x) res) (not (member? (caddr x) res))) (unreachable (cdr x) (cons (caddr x) res))])
+    res))
+
+
+
+(unreachable (sm-getrules QUIZ) (sm-getstartstate QUIZ))
