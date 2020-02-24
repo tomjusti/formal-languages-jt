9bf6c04b18323e5646b0a72124a46c52d426d984
tomjusti <tomjusti@shu.edu>
2020-02-24 16:50:47 -0500

    Created tests

--- Gargiulo fsm prefix automata.rkt
+++ Gargiulo fsm prefix automata.rkt
@@ -1,9 +1,11 @@
 #lang racket
+
 (require fsm)
+(require test-engine/racket-tests)
 
 
-; X (listof X) -> bool
-; return true if (eqv? X Y) where Y is an element of (listof X)
+;; member?: X, (list-of-X) -> boolean
+;; Purpose: to return true if (eqv? X Y) where Y is an element of (listof X)
 (define member?
   (lambda (element lst)
     (cond
@@ -11,8 +13,8 @@
       [(eqv? element (car lst)) #t]
       [else (member? element (cdr lst))])))
 
-; ndfa -> listof sym
-; return a list of all reachable states from the start state of ndfa
+;; reachable4: ndfa -> (list-of-sym)
+;; Purpose: to return a list of all reachable states from the start state of ndfa
 (define (reachable4 ndfa)
   (let*
       ((rules (sm-getrules ndfa)) ; list of rules of the ndfa
@@ -21,33 +23,37 @@
              (cond
                [(zero? i) visited]
                [(empty? r) (begin
-                             (set! i (- i 1)) ; whenever a pass is completed, i is decreased. At most, recursion can occur one time per state in the machine
-                             (reach-aux rules visited))] ; list of rules has been scanned, reset rules for another pass
-               [(and (member? (caar r) visited) (not (member? (caddar r) visited))) (reach-aux (cdr r) (cons (caddar r) visited))] ; found a member of visited pointing to a state not already visited
-               [else ; did not find member of visited
+                             (set! i (- i 1)) ;; whenever a pass is completed, i is decreased
+                                              ;; at most, recursion can occur one time per state in the machine
+                             (reach-aux rules visited))] ;; list of rules has been scanned, reset rules for another pass
+               ;; found a member of visited pointing to a state not already visited
+               [(and (member? (caar r) visited) (not (member? (caddar r) visited))) (reach-aux (cdr r) (cons (caddar r) visited))]
+               [else ;; did not find member of visited
                 (reach-aux (cdr r) visited)])))
-      (reach-aux rules (list (sm-getstart ndfa)))))) ; "visited" only contains the start state by default
+      (reach-aux rules (list (sm-getstart ndfa)))))) ;; "visited" only contains the start state by default
 
 
-; sym ndfa -> listof sym
-; return a list of all states in ndfa that are on a path to the final states of ndfa
-; (these are made final states in the prefix ndfa, as any state not on a path to final would not be reached by a valid prefix)
+;; path-to-final: sym, ndfa -> (list-of-sym)
+;; Purpose: to return a list of all states in ndfa that are on a path to the final states of ndfa
+;; these are made final states in the prefix ndfa, as any state not on a path to final would not be reached by a valid prefix
 (define (path-to-final ndfa)
   (let*
-      ((rules (sm-getrules ndfa)) ; list of rules of the ndfa
-       (i (length (sm-getstates ndfa))) ; used to limit recursion depth
+      ((rules (sm-getrules ndfa)) ;; list of rules of the ndfa
+       (i (length (sm-getstates ndfa))) ;; used to limit recursion depth
        (reachable (reachable4 ndfa)))
     (local ((define (reach-aux r visited)
               (cond
                 [(zero? i) visited]
                 [(empty? r) (begin
                               (set! i (- i 1))
-                              (reach-aux rules visited))] ; list of rules has been scanned, reset rules for another pass
+                              (reach-aux rules visited))] ;; list of rules has been scanned, reset rules for another pass
                 [(and (member? (caddar r) visited) (not (member? (caar r) visited)) (member? (caar r) reachable)) (reach-aux (cdr r) (cons (caar r) visited))] ; found a member of visited pointing to a state not already visited
-                [else ; did not find member of visited
+                [else ;; did not find member of visited
                  (reach-aux (cdr r) visited)])))
       (reach-aux rules (sm-getfinals ndfa)))))
 
+;; make-prefix-ndfa: ndfa -> ndfa
+;; Purpose: to create an ndfa that accepts all prefixes
 (define (make-prefix-ndfa ndfa)
   (let*
       ((new-finals (path-to-final ndfa)))
@@ -57,8 +63,7 @@
                new-finals
                (sm-getrules ndfa))))
 
-
-(define C
+(define A
   (make-ndfa '(Q0 Q1 Q2 Q3 Q4)
              '(a b c)
              'Q0
@@ -70,7 +75,26 @@
                (Q2 a Q4)
                (Q3 a Q4))))
 
-(define D
+(check-expect (sm-apply A '(a a a a a a a a a a a a b)) 'accept)
+(check-expect (sm-apply A '(a a a a a a a a a a a a b a)) 'accept)
+(check-expect (sm-apply A '(b)) 'accept)
+(check-expect (sm-apply A '(b a)) 'accept)
+(check-expect (sm-apply A '(a b)) 'accept)
+(check-expect (sm-apply A '(a b a)) 'accept)
+
+(check-expect (sm-apply (make-prefix-ndfa A) '(a a a a a a a a a a a a b)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa A) '(a a a a a a a a a a a a b a)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa A) '(b)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa A) '(b a)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa A) '(a b)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa A) '(a b a)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa A) '()) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa A) '(a a a a a a a a a a a a)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa A) '(a a a a a a a a a a a)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa A) '(a a a)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa A) '(a)) 'accept)
+
+(define B
   (make-ndfa '(Q0 Q1 Q2 Q3 Q4)
              '(a b)
              'Q0
@@ -80,6 +104,13 @@
                (Q2 b Q3)
                (Q3 a Q4))))
 
+(check-expect (sm-apply B '(a b b a)) 'accept)
+
+(check-expect (sm-apply (make-prefix-ndfa B) '(a b b a)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa B) '(a b b)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa B) '(a b)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa B) '(a)) 'accept)
+(check-expect (sm-apply (make-prefix-ndfa B) '()) 'accept)
 
 ; Let A be an ndfa accepting L
 ; Let A' be an ndfa accepting Prefix(L)
@@ -101,4 +132,4 @@
 ; F' = F U N(q, k)
 ; N(q, k) is all states from q to k
 
-
+(test)
