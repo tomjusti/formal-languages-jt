75530bd81e09b37cd6852175c153724c70387d44
gargiuer <gargiuer@shu.edu>
2020-03-30 18:21:50 -0400

    Update quiz4.rkt

--- quiz4.rkt
+++ quiz4.rkt
@@ -35,15 +35,18 @@
 
 ; Q0: no input  consumed, starting state
 ; Q1: 0 or more a's are pushed onto the stack
-; Q2: length of stack + b's in ci always between n and 2n
+; Q2: length of stack + b's in ci always between n and 2n (where n is number of a's in consumed-input)
 
+; in Q0, consumed-input and stack are both empty
 (define (Q0-INV consumed-input stack)
   (and (empty? consumed-input) (empty? stack)))
 
+; in Q1, stack and consumed-input are always length 0 or more
 (define (Q1-INV consumed-input stack)
   (and (>= (length consumed-input) 0)
-       (>= (length stack))))
+       (>= (length stack) 0)))
 
+; in Q2, length of stack + number of b's in consumed-input is always between n and 2n (where n is number of a's in consumed-input)
 (define (Q2-INV consumed-input stack)
   (and (>= (+ (length stack) (length (filter (lambda (x) (eq? x 'b)) consumed-input)))
            (length (filter (lambda (x) (eq? x 'a)) consumed-input)))
@@ -63,9 +66,8 @@
      ((M a ,EMP) (M(a)))
      ((F b (a)) (F ,EMP)))))
 
-; num b - num a in CI always 1 off length stack
-; len ci - len s
-
+; convert the given fsa to an ndpda
+; fsa -> ndpda
 (define (fsa->ndpda fsa)
   (make-ndpda
    (sm-getstates fsa)
@@ -75,12 +77,15 @@
    (sm-getfinals fsa)
    (converted-rules (sm-getrules fsa))))
 
+; convert a list of fsa rules to pda rules
+; list -> list
 (define (converted-rules lor)
   (cond
     [(empty? lor) '()]
     [else (cons (converted-rules-aux (car lor)) (converted-rules (cdr lor)))]))
 
 (define (converted-rules-aux rule) (list (list (car rule) (cadr rule) EMP) (list (caddr rule) EMP)))
+
 
 (define NDFA1
   (make-ndfa '(Q0 Q1)
@@ -93,6 +98,7 @@
               (Q1 a Q0)
               (Q1 b Q0))))
 
+
 (define NDPDA1
   (make-ndpda '(Q0 Q1)
             '(a b)
@@ -104,6 +110,7 @@
               ((Q0 b ,EMP) (Q1 ,EMP))
               ((Q1 a ,EMP) (Q0 ,EMP))
               ((Q1 b ,EMP) (Q0 ,EMP)))))
+
 
 (check-expect (sm-testequiv? NDFA1 (fsa->ndpda NDFA1)) #t)
 (check-expect (sm-testequiv? NDPDA1 (fsa->ndpda NDFA1)) #t)
